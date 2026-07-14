211cabf7c5ddef38367cd006c6ab1911141a71e2
M.S. Ojuba <76496417+SasheO@users.noreply.github.com>
2026-07-14 21:33:43 +0100

    added some tasks to dag

---
+++ dags/berka_elt.py
@@ -0,0 +1,84 @@
+# https://medium.com/@sant1/using-minio-with-docker-and-python-cbbad397cb5d
+import boto3
+import os
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+load_dotenv()
+
+MINIO_BUCKET_NAME = 'berka-raw-data-bucket'
+DATASETS_FOLDER = 'datasets'
+
+# TODO: add multiline comments to all functions
+def extract_source_data_from_kaggle():
+    import kaggle as kg # import kaggle ONLY after loading environment variables
+    kg.api.authenticate()
+    kg.api.dataset_download_files(dataset = "marceloventura/the-berka-dataset", path=DATASETS_FOLDER, unzip=True)
+    
+def stage_source_data_in_minio_bucket():
+    # TODO: clean up staged data from bucket at the end of task
+    s3_client = boto3.client('s3_client',
+                    endpoint_url='http://localhost:9000',
+                    aws_access_key_id=os.getenv("MINIO_ROOT_USER"),
+                    aws_secret_access_key=os.getenv("MINIO_ROOT_PASSWORD"))
+    # Create bucket if not exists
+    try:
+        s3_client.head_bucket(Bucket=MINIO_BUCKET_NAME) # will throw error if bucket doesn't exist
+    except:
+        s3_client.create_bucket(Bucket=MINIO_BUCKET_NAME)
+
+    path = DATASETS_FOLDER
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    for file in files:
+        # Upload a file to the bucket: https://docs.aws.amazon.com/boto3/latest/reference/services/s3_client/client/upload_file.html
+        s3_client.upload_file(os.path.join(path, file), MINIO_BUCKET_NAME, file)
+
+def ingest_staged_data_into_clickhouse_source_tables():
+    pass
+
+dag = DAG(
+    "berka_elt",
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        "depends_on_past": True,
+        "retries": 2,
+        "retry_delay": timedelta(minutes=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function, # or list of functions
+        # 'on_success_callback': some_other_function, # or list of functions
+        # 'on_retry_callback': another_function, # or list of functions
+        # 'sla_miss_callback': yet_another_function, # or list of functions
+        # 'on_skipped_callback': another_function, #or list of functions
+        'trigger_rule': 'all_success'
+    },
+    description="A dag which extracts, loads and transforms data from Berka financial dataset with DBT and Clickhouse",
+    schedule=timedelta(days=1),
+    start_date=datetime(2026, 7, 15),
+    catchup=False,
+    tags=["personal-project", "berka"],)
+
+with dag:
+    extract = PythonOperator(
+        task_id="extract_source_data_from_kaggle",
+        python_callable=extract_source_data_from_kaggle,
+        # op_kwargs={"key": "value"}  # Passes keyword arguments to the function
+    )
+
+    stage = PythonOperator(
+        task_id="stage_source_data_in_minio_bucket",
+        python_callable=stage_source_data_in_minio_bucket,
+    )
+
+    ingest_clickhouse = PythonOperator(
+        task_id="ingest_staged_data_into_clickhouse_source_tables",
+        python_callable=ingest_staged_data_into_clickhouse_source_tables,
+    )
+
+    extract >> stage >> ingest_clickhouse
